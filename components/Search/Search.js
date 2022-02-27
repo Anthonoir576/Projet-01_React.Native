8c3666f312d3e18e78c0d538948a1c960d4c9a54
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-02-27 11:04:10 +0100

    :FEAT: => style remise a niveau

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -15,7 +15,7 @@ const Search = () => {
         <View style={{marginTop: 0, margin: 1}}>
            <TextInput
             underlineColorAndroid='transparent' 
-            style={{height:40, borderColor: 'gray', borderWidth: .5, borderRadius: 5, backgroundColor: 'white', color: 'black'}}
+            style={{height:40, borderColor: 'transparent', borderWidth: 0, borderBottomWidth: .5, borderBottomColor: 'gray', opacity: .8, borderRadius: 5, backgroundColor: 'white', color: 'black'}}
             value={ville}
             onChangeText={(text) => {setVille(text)}}
            /> 
